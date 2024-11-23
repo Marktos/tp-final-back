2d6382828be158be2c9b2b00b6c7d8747278c77e
Marcos Neculman <marcosneculman040@gmail.com>
2024-11-22 22:29:06 -0300

    arreglando guard

--- src/modules/auth/guard/auth.guard.ts
+++ src/modules/auth/guard/auth.guard.ts
@@ -1,36 +1,36 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    private readonly jwtService: JwtService 
-  ){}
+  constructor(private readonly jwtService: JwtService) {}
 
-  async canActivate(
-    context: ExecutionContext,
-  ): Promise<boolean> {
-    const request = context.switchToHttp().getRequest()
-    const token = this.extractTokenFromHeader(request)
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
 
     if (!token) {
-      throw new UnauthorizedException()
+      throw new UnauthorizedException();
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.SECRET,
-      })
+      });
 
-      request.user = payload
-    }catch (error) {
-      throw new UnauthorizedException()
+      request.user = payload;
+    } catch (error) {
+      throw new UnauthorizedException();
     }
     return true;
   }
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const authorizationHeader = request.headers.authorization;
-    if (!authorizationHeader?.startsWith("bearer ")) return undefined;
-    return authorizationHeader.split(" ")[1];
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
